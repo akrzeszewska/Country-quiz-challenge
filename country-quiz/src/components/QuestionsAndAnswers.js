56682b822aa13c59dee25e6883a37a188aeae5aa
Aleksandra Krzeszewska <114503607+akrzeszewska@users.noreply.github.com>
2025-10-05 12:09:22 +0200

    Update QuestionsAndAnswers.js

--- country-quiz/src/components/QuestionsAndAnswers.js
+++ country-quiz/src/components/QuestionsAndAnswers.js
@@ -2,22 +2,34 @@ import { Question } from "./Question";
 import classes from "./QuestionsAndAnswers.module.css";
 import ImageUndrawAdventure from "../pictures/undraw_adventure_4hum_1.svg";
 import { Answer } from "./Answer";
-import { useState } from "react";
 
-export const QuestionsAndAnswers = ({questionToRead}) => {
-    const [question, setQuestion] = useState("");
-    const [answer, setAnswer] = useState("");
-    const [optionAbcd, setOptionAbcd] = useState("");
-
+export const QuestionsAndAnswers = ({ questionToRead }) => {
+  if (!questionToRead) {
     return (
-        <div className={classes.questionsandanswers}>
-            <img className={classes.image_undraw} src={ImageUndrawAdventure} />
-            <Question questionToRead={questionToRead} />
-            <Answer optionAbcd='A' answer='Poland' />
-            <Answer optionAbcd='B' answer='Germany' />
-            <Answer optionAbcd='C' answer='Italy' />
-            <Answer optionAbcd='D' answer='England' />
+      <div className={classes.questionsandanswers}>
+        <img className={classes.image_undraw} src={ImageUndrawAdventure} alt="" />
+        <div>Ładowanie…</div>
+      </div>
+    );
+  }
 
-        </div>
-    )
-}
+  // 3 błędne + 1 poprawna
+  const allAnswers = [
+    ...questionToRead.incorrect_answers,
+    questionToRead.correct_answer,
+  ];
+
+  // proste tasowanie (OK na start)
+  const shuffled = allAnswers.sort(() => Math.random() - 0.5);
+  const labels = ["A", "B", "C", "D"];
+
+  return (
+    <div className={classes.questionsandanswers}>
+      <img className={classes.image_undraw} src={ImageUndrawAdventure} alt="" />
+      <Question questionToRead={questionToRead} />
+      {shuffled.map((opt, i) => (
+        <Answer key={i} optionAbcd={labels[i]} answer={opt} />
+      ))}
+    </div>
+  );
+};
