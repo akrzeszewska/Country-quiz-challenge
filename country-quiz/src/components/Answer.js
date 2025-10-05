652c3d68d5c237063c6ecdc26418f33ee1ea45a6
Aleksandra Krzeszewska <114503607+akrzeszewska@users.noreply.github.com>
2025-10-05 12:28:04 +0200

    Update Answer.js

--- country-quiz/src/components/Answer.js
+++ country-quiz/src/components/Answer.js
@@ -1,14 +1,17 @@
 import classes from "./Answer.module.css";
 
-export const Answer = (props) => {
-    const {answer, optionAbcd} = props;
+export const Answer = ({ answer, optionAbcd, onSelect, selected }) => {
+  // selected = true/false – czy ta odpowiedź jest wybrana
 
-    return(
-        <div className={classes.answer}>
-            <div className={classes.abcd}>{optionAbcd}</div>
-            <div className={classes.country}>{answer}</div>
-        </div>
-    )
-}
-
-// addEventListener
+  return (
+    <button
+      type="button"
+      className={`${classes.answer} ${selected ? classes.selected : ""}`}
+      onClick={() => onSelect?.(answer)}
+      aria-pressed={selected}
+    >
+      <div className={classes.abcd}>{optionAbcd}</div>
+      <div className={classes.country}>{answer}</div>
+    </button>
+  );
+};
