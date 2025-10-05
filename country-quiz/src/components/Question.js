2ef4668b367e9d5bf82e2ede52b6ce6a8be8618d
Aleksandra Krzeszewska <114503607+akrzeszewska@users.noreply.github.com>
2025-10-05 12:01:17 +0200

    Update Question.js

--- country-quiz/src/components/Question.js
+++ country-quiz/src/components/Question.js
@@ -1,22 +1,11 @@
 import classes from "./Question.module.css";
 
-function decodeHtml(s) {
-  if (!s) return "";
-  return s
-    .replaceAll("&quot;", '"')
-    .replaceAll("&#039;", "'")
-    .replaceAll("&amp;", "&")
-    .replaceAll("&eacute;", "é")
-    .replaceAll("&lt;", "<")
-    .replaceAll("&gt;", ">");
-}
-
 export const Question = ({ questionToRead }) => {
   if (!questionToRead) return null; // jeszcze się ładuje
 
   return (
     <div className={classes.question}>
-      {decodeHtml(questionToRead.question)}
+      {(questionToRead.question)}
     </div>
   );
 };
